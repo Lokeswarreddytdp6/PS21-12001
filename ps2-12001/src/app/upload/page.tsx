3ec212cd9e62e6f6f5801db6b4ae4b3b04bd4b59
Shashank-Shourie <shashankdevulapally569@gmail.com>
2025-02-17 16:03:33 +0530

    modified:   ps2-12001/src/app/page.tsx 	modified:   ps2-12001/src/app/upload/page.tsx 	new file:   ps2-12001/src/components/navbar.tsx

--- ps2-12001/src/app/upload/page.tsx
+++ ps2-12001/src/app/upload/page.tsx
@@ -1,7 +1,9 @@
 import PdfViewer from "@/components/pdfViewer"
+import Navbar from "@/components/navbar"
 export default function Upload(){
     return(
         <>
+            <Navbar/>
             <h1>Upload the required files</h1>
             <PdfViewer/>
         </>
